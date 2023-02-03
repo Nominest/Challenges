2805078f6e2d32f6ca21e17258ce0f1dbb2a6a80
Morning <morning@lkhagva-erdenes-iMac.local>
2023-02-03 12:26:39 +0800

    added buttons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Pagination from "./components/Pagination";
 import "./index.css";
 import { Route, Routes } from "react-router-dom";
+import Page from "./components/Page";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route></Route>
+        <Route path="/page/:id" element={<Page />}></Route>
       </Routes>
       <Pagination />
     </div>
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,25 +1,38 @@
 import { useState } from "react";
 import PageButton from "./subcomponents/PageButton";
 import { Link, NavLink } from "react-router-dom";
+
 const pageNumbers = 100;
 
 export default function Pagination() {
   const [currentPage, setCurrentPAge] = useState(10);
-  // const [buttonsPerPAge] = useState(5);
-  // const indexOfLast = currentPage * buttonsPerPAge;
-  // const indexOfFirst = indexOfLast - buttonsPerPAge;
-  // const currentRecords = data.slice(indexOfFirst, indexOfLast);
+  const [buttonsPerPAge] = useState(5);
+  const indexOfLast = currentPage * buttonsPerPAge;
+  const indexOfFirst = indexOfLast - buttonsPerPAge;
+  // const currentRecords = pageNumbers.slice(indexOfFirst, indexOfLast);
 
   return (
     <div>
       <Link
+        to={`/page/${setCurrentPAge - 2}`}
+        onClick={() => setCurrentPAge(currentPage - 2)}
+      >
+        Previous
+      </Link>
+      <Link
+        to={`/page/${setCurrentPAge - 2}`}
+        onClick={() => setCurrentPAge(currentPage - 2)}
+      >
+        <PageButton btnName={currentPage - 2} btnClass={"pageBtn"} />
+      </Link>
+      <Link
         to={`/page/${setCurrentPAge - 1}`}
         onClick={() => setCurrentPAge(currentPage - 1)}
       >
-        <PageButton btnName={currentPage - 1} btnClass={"pageBtn-active"} />
+        <PageButton btnName={currentPage - 1} btnClass={"pageBtn"} />
       </Link>
       <span>
-        <PageButton btnName={currentPage} btnClass={"pageBtn"} />
+        <PageButton btnName={currentPage} btnClass={"pageBtn-active"} />
       </span>
       <Link
         to={`/page/${setCurrentPAge + 1}`}
@@ -27,7 +40,18 @@ export default function Pagination() {
       >
         <PageButton btnName={currentPage + 1} btnClass={"pageBtn"} />
       </Link>
-
+      <Link
+        to={`/page/${setCurrentPAge + 2}`}
+        onClick={() => setCurrentPAge(currentPage + 2)}
+      >
+        <PageButton btnName={currentPage + 2} btnClass={"pageBtn"} />
+      </Link>
+      <Link
+        to={`/page/${setCurrentPAge + 1}`}
+        onClick={() => setCurrentPAge(currentPage + 1)}
+      >
+        Next
+      </Link>
       {/* <PageButton btnName={"4"} btnClass={"pageBtn"} /> */}
     </div>
   );
